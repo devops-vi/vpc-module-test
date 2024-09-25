946e958e8a1ae1ce5cf05a01fe806a4084699ab8
Lav-Venkat <inukurthivenkatesh@gmail.com>
2024-09-25 20:59:49 +0530

    vpc test module

--- provider.tf
+++ provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.66.0"
+      version = "5.67.0"
     }
   }
 
   backend "s3" {
-    bucket = "81s-remote-state"
-    key    = "expense-vpcc"
-    region = "us-east-1"
-    dynamodb_table = "81s-locking"
+      bucket         = "laven-terrafom-state"
+      key            = "expense-vpc"
+      dynamodb_table = "terraform-practice"
+      region         = "us-east-1"
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "environment" {
 }
 
 variable "vpc_cidr" {
-    default = "10.0.0.0/16"
+    default = "100.223.0.0/16"
 }
 
 variable "common_tags" {
@@ -19,13 +19,17 @@ variable "common_tags" {
 }
 
 variable "public_subnet_cidrs" {
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+    default = ["100.223.1.0/24", "100.223.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
+    default = ["100.223.11.0/24", "100.223.12.0/24"]
 }
 
 variable "database_subnet_cidrs" {
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
+    default = ["100.223.21.0/24", "100.223.22.0/24"]
+}
+
+variable "is_peering_required" {
+    default = true
 }
